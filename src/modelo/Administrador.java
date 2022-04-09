c1e3cc65f7049c5a0755e0c5f37054db69df2e7f
Enrique Herrera <bycrashdash@gmail.com>
2022-04-09 15:14:31 -0500

    se agrego modelo administrador

---
+++ src/modelo/Administrador.java
@@ -0,0 +1,44 @@
+package modelo;
+
+public class Administrador {
+	private String usuario;
+	private String nombre;
+	private String contrasenia;
+
+	public Administrador(String usuario, String nombre, String contrasenia) {
+		super();
+		this.usuario = usuario;
+		this.nombre = nombre;
+		this.contrasenia = contrasenia;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getContrasenia() {
+		return contrasenia;
+	}
+
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+
+	@Override
+	public String toString() {
+		return "Administrador [usuario=" + usuario + ", nombre=" + nombre + ", contrasenia=" + contrasenia + "]";
+	}
+
+}
