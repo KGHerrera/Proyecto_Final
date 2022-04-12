ebb2a0f53d8dc32d8e4a319bfce6d7cd8bc6227d
Enrique Herrera <bycrashdash@gmail.com>
2022-04-12 12:11:27 -0500

    se agregaron metodo de altas bajas cambios y consultas para empleados

--- src/conexion/ConexionBD.java
+++ src/conexion/ConexionBD.java
@@ -51,177 +51,175 @@ public class ConexionBD extends Thread {
 			e.printStackTrace();
 		}
 	}
-
+	
 	// verificacion de usuario
 
-	public static ResultSet consultarUsuario(Usuario u) {
-		try {
-			String consulta = "select * from usuarios where usuario=? AND contrasenia=?";
-			pstm = conexion.prepareStatement(consulta);
-			pstm.setString(1, u.getUsuario());
-			pstm.setString(2, u.getContrasenia());
+		public static ResultSet consultarUsuario(Usuario u) {
+			try {
+				String consulta = "select * from usuarios where usuario=? AND contrasenia=?";
+				pstm = conexion.prepareStatement(consulta);
+				pstm.setString(1, u.getUsuario());
+				pstm.setString(2, u.getContrasenia());
 
-			rs = pstm.executeQuery();
+				rs = pstm.executeQuery();
 
-		} catch (Exception ex) {
-			System.out.println(ex.toString());
+			} catch (Exception ex) {
+				System.out.println(ex.toString());
+			}
+
+			return rs;
 		}
 
-		return rs;
-	}
+		public static boolean altaEmpleado(Empleado e) {
+			try {
 
-	public static boolean altaEmpleado(Empleado e) {
-		try {
+				String filtro = "insert into empleados values (null,?,?,?,?)";
+				pstm = conexion.prepareStatement(filtro);
 
-			String filtro = "insert into empleados values (null,?,?,?,?)";
-			pstm = conexion.prepareStatement(filtro);
+				pstm.setString(1, e.getNombre());
+				pstm.setString(2, e.getApellido());
+				pstm.setDouble(3, e.getSalario());
+				pstm.setString(4, e.getCargo());
+				pstm.executeUpdate();
+				return true;
 
-			pstm.setString(1, e.getNombre());
-			pstm.setString(2, e.getApellido());
-			pstm.setDouble(3, e.getSalario());
-			pstm.setString(4, e.getCargo());
-			pstm.executeUpdate();
-			return true;
-
-		} catch (SQLException error) {
-			error.printStackTrace();
+			} catch (SQLException error) {
+				error.printStackTrace();
+			}
+			return false;
 		}
-		return false;
-	}
 
-	public static boolean bajaEmpleado(Empleado e) {
-		try {
-			String consulta = "delete from empleados where ";
-			int pos = 1;
-			consulta = generarConsultaEmpleado(e);
+		public static boolean bajaEmpleado(Empleado e) {
+			try {
+				String consulta = "delete from empleados where ";
+				int pos = 1;
+				consulta = generarConsultaEmpleado(e);
 
-			pstm = conexion.prepareStatement(consulta);
+				pstm = conexion.prepareStatement(consulta);
 
+				if (e.getIdEmpleado() != 0) {
+					pstm.setInt(pos, e.getIdEmpleado());
+					pos++;
+				}
+
+				if (e.getNombre() != null) {
+					pstm.setString(pos, e.getNombre());
+					pos++;
+				}
+
+				if (!e.getApellido().equals("null")) {
+					pstm.setString(pos, e.getApellido());
+					pos++;
+				}
+
+				if (e.getSalario() != 0.0) {
+					pstm.setDouble(pos, e.getSalario());
+					pos++;
+				}
+
+				if (e.getCargo() != null) {
+					pstm.setString(pos, e.getCargo());
+					pos++;
+				}
+
+				pstm.executeUpdate();
+
+				return true;
+
+			} catch (Exception ex) {
+				System.out.println(ex.toString());
+			}
+			return false;
+		}
+
+		public static boolean cambioEmpleado(Empleado e) {
+			try {
+				String consulta = "update empleados set nombre=?, apellido=?, =?, salario=?, cargo=? WHERE id_cliente=?";
+				pstm = conexion.prepareStatement(consulta);
+				pstm.setString(1, e.getNombre());
+				pstm.setString(2, e.getApellido());
+				pstm.setDouble(3, e.getSalario());
+				pstm.setString(4, e.getCargo());
+				pstm.setInt(5, e.getIdEmpleado());
+
+				pstm.executeUpdate();
+
+				return true;
+
+			} catch (Exception ex) {
+				System.out.println(ex.toString());
+			}
+			return false;
+		}
+
+		public static ResultSet consultaEmpleado(Empleado e) {
+			try {
+				String consulta = "select * from empleados where ";
+				int pos = 1;
+				consulta = generarConsultaEmpleado(e);
+
+				pstm = conexion.prepareStatement(consulta);
+
+				if (e.getIdEmpleado() != 0) {
+					pstm.setInt(pos, e.getIdEmpleado());
+					pos++;
+				}
+
+				if (e.getNombre() != null) {
+					pstm.setString(pos, e.getNombre());
+					pos++;
+				}
+
+				if (!e.getApellido().equals("null")) {
+					pstm.setString(pos, e.getApellido());
+					pos++;
+				}
+
+				if (e.getSalario() != 0.0) {
+					pstm.setDouble(pos, e.getSalario());
+					pos++;
+				}
+
+				if (e.getCargo() != null) {
+					pstm.setString(pos, e.getCargo());
+					pos++;
+				}
+
+				rs = pstm.executeQuery();
+
+			} catch (Exception ex) {
+				System.out.println(ex.toString());
+			}
+			return rs;
+		}
+
+		public static String generarConsultaEmpleado(Empleado e) {
+
+			String consulta = "";
 			if (e.getIdEmpleado() != 0) {
-				pstm.setInt(pos, e.getIdEmpleado());
-				pos++;
+				consulta += "id_empleado=? and ";
 			}
 
 			if (e.getNombre() != null) {
-				pstm.setString(pos, e.getNombre());
-				pos++;
+				consulta += "nombre=? and ";
 			}
 
 			if (!e.getApellido().equals("null")) {
-				pstm.setString(pos, e.getApellido());
-				pos++;
+				consulta += "apellido=? and ";
 			}
 
 			if (e.getSalario() != 0.0) {
-				pstm.setDouble(pos, e.getSalario());
-				pos++;
+				consulta += "salario=? and ";
 			}
 
 			if (e.getCargo() != null) {
-				pstm.setString(pos, e.getCargo());
-				pos++;
+				consulta += "cargo=? and ";
 			}
 
-			pstm.executeUpdate();
+			if ((consulta.substring(consulta.length() - 4, consulta.length()).equals("and ")))
+				consulta = consulta.substring(0, consulta.length() - 4);
 
-			return true;
-
-		} catch (Exception ex) {
-			System.out.println(ex.toString());
+			return consulta;
 		}
-		return false;
-	}
-
-	public static boolean cambioEmpleado(Empleado e) {
-		try {
-			String consulta = "update empleados set nombre=?, apellido=?, =?, salario=?, cargo=? WHERE id_cliente=?";
-			pstm = conexion.prepareStatement(consulta);
-			pstm.setString(1, e.getNombre());
-			pstm.setString(2, e.getApellido());
-			pstm.setDouble(3, e.getSalario());
-			pstm.setString(4, e.getCargo());
-			pstm.setInt(5, e.getIdEmpleado());
-
-			pstm.executeUpdate();
-
-			return true;
-
-		} catch (Exception ex) {
-			System.out.println(ex.toString());
-		}
-		return false;
-	}
-
-	public static ResultSet consultaEmpleado(Empleado e) {
-		try {
-			String consulta = "select * from empleados where ";
-			int pos = 1;
-			consulta = generarConsultaEmpleado(e);
-
-			pstm = conexion.prepareStatement(consulta);
-
-			if (e.getIdEmpleado() != 0) {
-				pstm.setInt(pos, e.getIdEmpleado());
-				pos++;
-			}
-
-			if (e.getNombre() != null) {
-				pstm.setString(pos, e.getNombre());
-				pos++;
-			}
-
-			if (!e.getApellido().equals("null")) {
-				pstm.setString(pos, e.getApellido());
-				pos++;
-			}
-
-			if (e.getSalario() != 0.0) {
-				pstm.setDouble(pos, e.getSalario());
-				pos++;
-			}
-
-			if (e.getCargo() != null) {
-				pstm.setString(pos, e.getCargo());
-				pos++;
-			}
-
-			rs = pstm.executeQuery();
-
-		} catch (Exception ex) {
-			System.out.println(ex.toString());
-		}
-		return rs;
-	}
-
-	public static String generarConsultaEmpleado(Empleado e) {
-
-		String consulta = "";
-		if (e.getIdEmpleado() != 0) {
-			consulta += "id_empleado=? and ";
-		}
-
-		if (e.getNombre() != null) {
-			consulta += "nombre=? and ";
-		}
-
-		if (!e.getApellido().equals("null")) {
-			consulta += "apellido=? and ";
-		}
-
-		if (e.getSalario() != 0.0) {
-			consulta += "salario=? and ";
-		}
-
-		if (e.getCargo() != null) {
-			consulta += "cargo=? and ";
-		}
-
-		if ((consulta.substring(consulta.length() - 4, consulta.length()).equals("and ")))
-			consulta = consulta.substring(0, consulta.length() - 4);
-
-		return consulta;
-	}
-
-	
 
 }
